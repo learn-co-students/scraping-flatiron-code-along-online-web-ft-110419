c22ae8c76dfce44e17cbd0d1444f655fc2d22c5f
Kenneth Andrews <kennethandrews@protonmail.com>
2019-11-27 13:02:37 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,25 @@
+class Course
 
+  @@all = []
+
+
+  attr_accessor :title, :description, :schedule
+
+
+  def initialize
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+    
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
+end
 
